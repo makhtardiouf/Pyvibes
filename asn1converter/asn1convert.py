c4bd7841a5cbba8352f19f645f6f9b488e3e43e7
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-04-11 22:32:32 +0900

    ASN1 converter

--- asn1converter/asn1convert.py
+++ asn1converter/asn1convert.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """
-Parse a file containing ASN1 pseudo-code and prep it for C++ conversion. Shares some similarities with asn1c, but this version focus on the latest 3GPP TS docs extraction.
+Parse a file containing ASN1 pseudo-code and prep it for C++ conversion.
+Shares some similarities with asn1c, but this version focus on the latest 3GPP TS docs extraction.
 
 Formats the output file with 'astyle'.
+Ref: http://www.itu.int/en/ITU-T/asn1/Pages/introduction.aspx
+Reviewed-by: Makhtar Diouf <makhtar.diouf@gmail.com>
 $Id: asn1convert.py, 9ddcffa7b4df  makhtar $
 """
 import sys
@@ -11,15 +14,15 @@ import re
 
 __author__ = "Makhtar Diouf"
 
-inp = ''
+infile = ''
 if len(sys.argv) < 2:
     # input("Specify the ASN file to convert: ")
-    inp = "asn1test.txt"
+    infile = "asn1test.txt"
 else:
-    inp = sys.argv[1]
+    infile = sys.argv[1]
 
-fp = open(inp)
-print("Reading file ", fp.name)
+inp = open(infile)
+print("Reading file ", inp.name)
 
 # Symbols table
 opts = {
@@ -27,27 +30,29 @@ opts = {
     "CHOICE": " enum ",
     "ENUMERATED": " enum ",
     "BOOLEAN": " bool ",
+    "BIT": "std::bitset<1>",
     "INTE": " int ",
     "STRING": " string ",
     "OPTIONAL": '',
     "Need ON": " ",
     "Need OP": " ",
     "NULL": 'typedef ',
+    "END": '// END',
     "...": "\n\n",
     "[[": " \n",
     "]]": " \n",
     "::=": " ",
-    #"--": " // ",
+    "--": " // ",
     "-": '',
     ",": ' ',
     "\t": ''
 }
 
 try:
-    line = fp.readline()
-    outp = open(fp.name + ".h", "w")
+    line = inp.readline()
+    outp = open(inp.name + ".h", "w")
     # Foward declarations header file
-    outp2 = open(fp.name + "_typedefs.h", "w")
+    outp2 = open(inp.name + "_typedefs.h", "w")
     outp.write('#include "' + outp2.name + '"\n')
 
     typedef_list = []
@@ -59,18 +64,19 @@ try:
         if "ASN1START" in _line:
             inAsn = True
             outp.write("// " + _line.strip() + "\n")
-            line = fp.readline()
+            line = inp.readline()
 
         elif "ASN1STOP" in _line:
             inAsn = False
         return inAsn
 
+    #d1 = False
     while line != '':
         inAsn = checkAsn(line)
         if not inAsn:
             # Comment out non-ASN text
             outp.write("// " + line.strip() + "\n")
-            line = fp.readline()
+            line = inp.readline()
             continue
 
         tmp = line.split("\t")
@@ -88,14 +94,28 @@ try:
                 # e.g. INTEGER (0..28)
                 line += (s[:4]) + " "
 
+        # Enum in previous line
+        #if d1:
+        #    opts[','] = ','
+
         d1 = False
         d2 = False
         d3 = False  # struct in previous line
 
         if "{" in line:
             d1 = True
-            line = re.sub("{", " ", line)
-            line = str(line) + " {"
+            if 'ENUMERATE' in line:
+                opts[','] = ','
+            # Handle long enums, and structs
+            tmp = line.split("{")
+            line = tmp[0]
+            tmp = tmp[1].split(',')
+            if 'SEQ' in line:
+                line += tmp[len(tmp)-1] + " { \n"
+            else:
+                line += tmp[len(tmp)-1] + " { \n\t" + tmp[0] + ", "
+
+            line += ', '.join(tmp[1:len(tmp)-2])
 
         elif "}" in line:
             d2 = True
@@ -103,6 +123,7 @@ try:
         elif "struct" in line:
             d3 = True
             line = "\t" + str(line)
+
 
         # Replace items present in the symbols table
         for k in opts:
@@ -128,19 +149,28 @@ try:
             outp.write("\t" + line + "\n")
         else:
             outp.write("\t" + line + ";\n")
-        line = fp.readline()
+        line = inp.readline()
+        # Reset d1
+        opts[','] = ' '
 
 except Exception:
     print("Aborting, regex error ", sys.exc_info()[0])
-    # raise
+    raise
 
 finally:
-    fp.close()
+    inp.close()
     os.system("astyle --style=gnu " + outp.name)
-    print("See output file: ", outp.name)
+    print("See output files: ", outp.name, outp2.name)
 
-    outp2.write("\n// Forward declarations for " + outp.name)
+    outp2.write("\n\n// Forward declarations for " + outp.name)
     outp.close()
     for s in typedef_list:
-        outp2.write("\ntypedef " + s + ";")
+        # print(s, s.isalnum(), s.isspace())
+        # if s.isalnum():
+            outp2.write("\ntypedef " + s + ";")
+
+    # Remove empty typedefs
     outp2.close()
+    os.system("sed -i 's/typedef ;/ /g' " + outp2.name)
+    print(len(typedef_list), " typedefs generated. Check if any are missing")
+    
--- asn1converter/asn1test.txt_typedefs.h
+++ asn1converter/asn1test.txt_typedefs.h
@@ -1,11 +1,12 @@
 
+
 // Forward declarations for asn1test.txt.h
 typedef CRNTI;
 typedef MACMainConfigSLr12;
 typedef SLCommResourcePoolr12;
-typedef ;
+ 
 typedef SLTxPoolToReleaseListr12;
-typedef SLCommTxPoolToAddModListr12;
+ 
 typedef LogicalChGroupInfoListr13;
 typedef SLTxPoolToReleaseListExtr13;
-typedef ;
+ 
