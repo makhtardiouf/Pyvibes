dc6b93a793a36f922e9e8711f0a7db64855db448
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-04-12 15:25:34 +0900

    Balanced tokens checker

---
+++ symbalance.py
@@ -0,0 +1,41 @@
+#
+# Balanced tokens checker
+# More general solution than http://interactivepython.org/runestone/static/pythonds/BasicDS/SimpleBalancedParentheses.html
+# $Id$
+__author__ = "Makhtar Diouf"
+
+symbols = {'{': '}', '(': ')', '[': ']'}
+
+def isbalanced(_target):
+    # Opening
+    ostack = list()
+    # Matching closing symbol
+    cstack = list()
+    for s in list(_target):
+        # Ignore characters not in the table
+        if s in symbols:
+            ostack.append(s)
+        elif s in symbols.values():
+            cstack.append(s)
+
+    #print(ostack, cstack)
+    if len(ostack) == len(cstack):
+        i = 0
+        for s in ostack:
+            print(s, symbols[s])
+            if not (ostack.count(s) == cstack.count(symbols[s])):
+                return False
+            elif ostack.index(s, i) == cstack.index(symbols[s], i):
+                print("Match: ", s, symbols[s])
+                #break #continue
+            else:
+                return False
+            i += 1
+        return True
+    return False
+
+
+print(isbalanced('((([abc])))'))
+print(isbalanced('ac(()'))
+print(isbalanced(')xya('))
+
