98e4c7388e214886d6ef50b77baa48d9184c6135
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-05-04 21:09:12 +0900

    Ordered lists

--- list_ordered.py
+++ list_ordered.py
@@ -1,83 +1,106 @@
 # OrderedList implementation
+# Data structure streamlined and augmented by Makhtar Diouf
 # http://interactivepython.org/runestone/static/pythonds/BasicDS/ImplementinganOrderedList.html
 from random import randint
 
+
 class Node:
-    def __init__(self,initdata):
+
+    def __init__(self, initdata):
         self.data = initdata
         self.next = None
+    
+    # Operator overloading (>)    
+    def __gt__(self, Node):
+        return self.data > Node.data
 
-    def getData(self):
-        return self.data
+# Inherit list()
+class OrderedList(list):
 
-    def getNext(self):
-        return self.next
-
-    def setData(self,newdata):
-        self.data = newdata
-
-    def setNext(self,newnext):
-        self.next = newnext
-
-
-class OrderedList:
     def __init__(self):
         self.head = None
+        self.idx = 0
+        self.sz = 0
 
-    def search(self,item):
+    def index(self, item):
+        exists = self.search(item)
+        if exists:
+            return self.idx
+        return None
+
+    def search(self, item):
+        self.idx = 0
         current = self.head
         found = False
         stop = False
         while current != None and not found and not stop:
-            if current.getData() == item:
+            if current.data == item:
                 found = True
             else:
-                if current.getData() > item:
+                if current.data > item:
                     stop = True
                 else:
-                    current = current.getNext()
+                    current = current.next
+                    self.idx += 1
 
         return found
 
-    def add(self,item):
+    def swap(self, n1, n2):
+        tmp = n1.data
+        n1.data = n2.data
+        n2.data = tmp
+    
+    def add(self, item):
+        self.idx = 0
         current = self.head
-        previous = None
+        prev = None
         stop = False
+        tmp = Node(item)
         while current != None and not stop:
-            if current.getData() > item:
+            if current > tmp:
                 stop = True
             else:
-                previous = current
-                current = current.getNext()
-
-        temp = Node(item)
-        if previous == None:
-            temp.setNext(self.head)
-            self.head = temp
+                prev = current
+                current = current.next
+                self.idx += 1
+        
+        if prev == None:
+            tmp.next = self.head
+            self.head = tmp
         else:
-            temp.setNext(current)
-            previous.setNext(temp)
+            tmp.next = current
+            prev.next = tmp
+            
+        self.sz += 1
+        self.idx += 1
+        if (current) and (current.data > item):
+            self.swap(current, tmp)
+            self.insert(self.idx-2, item)
+        else:
+            self.insert(self.idx, item)
 
     def isEmpty(self):
         return self.head == None
 
-    def size(self):
-        current = self.head
-        count = 0
-        while current != None:
-            count = count + 1
-            current = current.getNext()
-
-        return count
+    def size(self):        
+        return self.sz
 
 
-mylist = OrderedList()
-mylist.add(31)
-mylist.add(93)
+l = OrderedList()
+l.add(150)
+l.add(93)
 for i in range(10):
-    mylist.add(randint(i, 200))
+    l.add(randint(i, 200))
 
-print(mylist.size())
-print(mylist.search(93))
-print(mylist.search(100))
-print(mylist.size())
+print(l.size())
+print(l.search(93))
+print(l.search(100))
+print("Deleting idx: ", l.index(93))
+# del(l.index(31))
+print(l.size())
+print(l, "\n\n")
+
+l = sorted(l)
+for i in range(len(l)):
+    print(l[i], end=', ')
+print()
