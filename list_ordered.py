5cb36f7aa973aaac07192af77130209c3b0d3b24
Makhtar Diouf <makhtar.diouf@gmail.com>
2016-05-04 21:08:17 +0900

    DS ordered lists

---
+++ list_ordered.py
@@ -0,0 +1,83 @@
+# OrderedList implementation
+# http://interactivepython.org/runestone/static/pythonds/BasicDS/ImplementinganOrderedList.html
+from random import randint
+
+class Node:
+    def __init__(self,initdata):
+        self.data = initdata
+        self.next = None
+
+    def getData(self):
+        return self.data
+
+    def getNext(self):
+        return self.next
+
+    def setData(self,newdata):
+        self.data = newdata
+
+    def setNext(self,newnext):
+        self.next = newnext
+
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
+
+    def search(self,item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            else:
+                if current.getData() > item:
+                    stop = True
+                else:
+                    current = current.getNext()
+
+        return found
+
+    def add(self,item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+
+    def isEmpty(self):
+        return self.head == None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.getNext()
+
+        return count
+
+
+mylist = OrderedList()
+mylist.add(31)
+mylist.add(93)
+for i in range(10):
+    mylist.add(randint(i, 200))
+
+print(mylist.size())
+print(mylist.search(93))
+print(mylist.search(100))
+print(mylist.size())
