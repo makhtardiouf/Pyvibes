b264cf69bab454fa0fbf24dcdc562e88d76652a5
Makhtar <makhtar.diouf@gmail.com>
2021-11-11 11:31:11 +0000

    Cleanup Django folders

--- django_demo02/polls/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Question, Choice
-
-# Register your models here, to enable CRUD with the admin UI
-
-admin.site.register(Question)
-admin.site.register(Choice)
